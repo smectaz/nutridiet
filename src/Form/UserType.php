17f735a4e559413194e4db5a845ba856aefb2381
smectaz <90684643+smectaz@users.noreply.github.com>
2022-11-07 00:59:37 +0100

    update bouton modify password

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -58,7 +58,8 @@ class UserType extends AbstractType
             ->add('submit', SubmitType::class, [
                 'attr' => [
                     'class' => 'btn btn-primary mt-4'
-                ]
+                ],
+                'label' => 'modifier le profil'
             ]);
     }
 
